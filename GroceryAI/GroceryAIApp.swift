473fffb065c27c845cedd5d4fb1f75e5fa9a1868
david <davidsmiron@gmail.com>
2025-02-28 19:49:11 +0200

    Initial Commit

---
+++ GroceryAI/GroceryAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAIApp.swift
+//  GroceryAI
+//
+//  Created by David Miron on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryAITests/GroceryAITests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAITests.swift
+//  GroceryAITests
+//
+//  Created by David Miron on 28.02.2025.
+//
+
+import Testing
+@testable import GroceryAI
+
+struct GroceryAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
